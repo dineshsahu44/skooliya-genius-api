2a34a00be386c4ad70f40b40f8b092e7bf2707c8
dineshsahu44 <solutionssharda@gmail.com>
2022-08-18 02:21:15 +0530

    O

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ use App\Http\Controllers\CommentController;
 */
 Route::get('/', [StaticController::class,'show']);
 Route::middleware('checkurl')->group(function(){
-    Route::post('/loginapi.php', [AuthController::class,'login']);
+    Route::any('/loginapi.php', [AuthController::class,'login']);
     Route::group(['middleware' => 'auth.verify'], function(){
         Route::post('/checkuspassapi.php', [AuthController::class,'checkUsPass']);
         Route::post('/changepasswordapi.php', [AuthController::class,'changePassword']);
